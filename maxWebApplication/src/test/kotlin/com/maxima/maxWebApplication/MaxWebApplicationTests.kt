c128b5c3951a028b9354a17e0c9ee6cd923934a1
kengy <kengyysk8@gmail.com>
2020-07-30 20:44:14 -0300

    Iniciando Projeto

---
+++ maxWebApplication/src/test/kotlin/com/maxima/maxWebApplication/MaxWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.maxima.maxWebApplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaxWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
